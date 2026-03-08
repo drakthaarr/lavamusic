d14ea7b6061d743d3ce548d8dee5c98577482870
Viacheslav D. <viacheslav.dudnyk@gmail.com>
2026-03-08 01:04:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True
 
-bot = commands.Bot(command_prefix="a!", intents=intents)
+bot = commands.Bot(command_prefix="a!", intents=intents, help_command=None)
 
 # ─────────────────────────────────────────────
 #  Режимы зацикливания
